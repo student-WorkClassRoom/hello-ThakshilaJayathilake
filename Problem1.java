96b9a05dd85ca41079654d49c3833a82c3052238
Thakshila_Jayathilake <e18156@eng.pdn.ac.lk>
2022-06-18 19:03:43 +0530

    corrected

--- Problem1.java
+++ Problem1.java
@@ -1,7 +1,6 @@
-import java.io.InputStream;
-import java.io.OutputStream;
-
-public static void main(String[] args) {
-    // print "hello world" here
-    System.out.println("hello world");
+class Problem1 {
+    public static void main(String[] args) {
+        // print "hello world" here
+        System.out.println("hello world");
+    }
 }
